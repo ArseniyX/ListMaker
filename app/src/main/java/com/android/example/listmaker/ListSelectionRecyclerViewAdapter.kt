18cb621e22c8ec234e96bdab87dc0ccb9b1cdc22
Arseniy <jkl@usa.com>
2019-12-16 23:26:24 +0200

    Filling in the blanks

--- app/src/main/java/com/android/example/listmaker/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/android/example/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -5,7 +5,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewHolder>() {
 
-    val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
+    private val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
